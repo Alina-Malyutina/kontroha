d583e42b6da87df3074b43cb641d8ac02939359f
Alina Malyutina <linamalutina1@gmail.com>
2022-12-07 13:47:20 +0300

    add program

---
+++ Program/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("This program assepts size of array and strings for it.");
+Console.WriteLine("After that program print only such lines there quantity of symbols less than or equal to 3");
+Console.Write("Enter quantity of strings, that you going to enter: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string [] ArrayStart = new string [N];
+
+for (int i = 0; i < ArrayStart.Length; i++)
+{
+    Console.Write("Enter string: ");
+    ArrayStart[i] = Console.ReadLine()!;
+}
+
+string [] ArrayFinal = new string [N];
+int temp = 0;
+
+for (int i = 0; i < ArrayStart.Length; i++)
+{
+    if (ArrayStart[i].Length <= 3)
+    {
+        ArrayFinal[temp] = ArrayStart[i];
+        temp++;
+    }
+}
+
+Console.Write("[");
+for (int i = 0; i < ArrayFinal.Length; i++)
+{
+    if (ArrayFinal[i] is not "")
+    {
+        Console.Write($"{ArrayFinal[i]} ,");
+    }
+}
+
+Console.Write("]");
